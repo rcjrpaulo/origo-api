cfeeee0242ccda0cb67d574641bd6a944ffce318
Paulo Cardoso <paulo@agily.com.b>
2021-03-14 15:39:38 -0300

    formRequest

---
+++ app/Http/Requests/Cliente/StoreClienteRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Cliente;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreClienteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'planos' => ['required', 'array'],
+            'nome' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:clientes'],
+            'telefone' => ['required', 'string'],
+            'estado' => ['required', 'string'],
+            'cidade' => ['required', 'string'],
+            'data_de_nascimento' => ['required'],
+        ];
+    }
+}
