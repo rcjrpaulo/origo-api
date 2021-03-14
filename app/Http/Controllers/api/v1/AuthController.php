acfc4a120d767e5d5cac27c5d338011fea04756b
Paulo Cardoso <paulo@agily.com.b>
2021-03-14 18:30:57 -0300

    fix return auth

--- app/Http/Controllers/api/v1/AuthController.php
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
             'password' => Hash::make($request->password)
         ]);
 
-        return response()->json($user);
+        return response()->json(['user' => $user]);
     }
 
     public function login(LoginRequest $request)
